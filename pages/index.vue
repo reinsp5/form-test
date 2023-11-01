f5b77540236f93193b154b92d70e6111309d19ac
DESKTOP-2HGUL02\reinsp5 <sk.reinsp5@gmail.com>
2023-11-02 00:17:21 +0900

    formDataを使用せずにJSONでのやり取りにした

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,38 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const name = ref("");
+const email = ref("");
+const message = ref("");
+
+const sendForm = async () => {
+  const formData = new FormData();
+
+  const { data } = await useFetch("/api/sendForm", {
+    method: "POST",
+    body: {
+      name: name.value,
+      email: email.value,
+      message: message.value,
+    },
+  });
+
+  console.log(data.value);
+};
+</script>
 
 <template>
-  <form action="/api/sendForm" method="POST">
+  <form @submit.prevent="sendForm">
     <label for="name">氏名：</label>
-    <input type="text" name="name" />
+    <input type="text" name="name" v-model="name" />
     <label for="email">メールアドレス：</label>
-    <input type="text" name="email" />
-    <label for="body"></label>
-    <textarea name="message" id="" cols="30" rows="10"></textarea>
+    <input type="text" name="email" v-model="email" />
+    <label for="message"></label>
+    <textarea
+      name="message"
+      id=""
+      cols="30"
+      rows="10"
+      v-model="message"
+    ></textarea>
     <button type="submit">送信</button>
   </form>
 </template>
--- server/api/sendForm.ts
+++ server/api/sendForm.ts
@@ -1,3 +1,64 @@
 export default defineEventHandler(async (event) => {
-  return "Hello."
+  const body = await readBody(event);
+  const recipientEmail = "sk.reinsp5@gmail.com"; // 自分のメールアドレス
+  const sender = "お試しフォーム太郎"; // 自分の名前や会社名
+  const senderEmail = "no-replay@reinsp5.com"; // 送信に使うメールアドレス（ドメインが同じなら適当でも可）
+  const name = body.name; // フォームデータの中身
+  const email = body.email; // フォームデータの中身
+  const message = body.message; // フォームデータの中身
+
+  // お問い合わせ内容をサイト運営者に送信
+  const toAdminRes = await fetch("https://api.mailchannels.net/tx/v1/send", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({
+      personalizations: [
+        {
+          to: [{ email: recipientEmail }],
+        },
+      ],
+      from: { email: senderEmail, name: sender },
+      subject: "お問い合わせがありました",
+      content: [
+        {
+          type: "text/plain",
+          value: `【お名前】\n${name}様\n【メールアドレス】\n${email}\n【お問い合わせ内容】\n${message}`,
+        },
+      ],
+    }),
+  });
+
+  // お客様へ自動返信メール
+  if (toAdminRes.ok) {
+    await fetch("https://api.mailchannels.net/tx/v1/send", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        personalizations: [
+          {
+            to: [{ email }],
+          },
+        ],
+        from: { email: senderEmail, name: sender },
+        subject: "お問い合わせありがとうございます",
+        content: [
+          {
+            type: "text/plain",
+            value: `${name}様\nこの度はお問い合わせいただきありがとうございます。(略)\n【お問い合わせ内容】\n${message}`,
+          },
+        ],
+      }),
+    });
+
+    return "送信に成功しました！";
+  }
+
+  throw createError({
+    statusCode: 500,
+    statusMessage: "送信に失敗しました",
+  });
 });
