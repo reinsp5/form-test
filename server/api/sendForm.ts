1d7efd82b1aea222207587605420122e8c5c16b4
レイン <sk.reinsp5@gmail.com>
2023-11-02 11:35:41 +0900

    変更した

--- server/api/sendForm.ts
+++ server/api/sendForm.ts
@@ -8,9 +8,36 @@ export default defineEventHandler(async (event) => {
   const email = body.email; // フォームデータの中身
   const message = body.message; // フォームデータの中身
 
-  try {
-    // お問い合わせ内容をサイト運営者に送信
-    const toAdminRes = await fetch("https://api.mailchannels.net/tx/v1/send", {
+  // お問い合わせ内容をサイト運営者に送信
+  const toAdminRes = await fetch("https://api.mailchannels.net/tx/v1/send", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({
+      personalizations: [
+        {
+          to: [{ email: recipientEmail }],
+        },
+      ],
+      from: { email: senderEmail, name: sender },
+      subject: "お問い合わせがありました",
+      content: [
+        {
+          type: "text/plain",
+          value: `【お名前】\n${name}様\n【メールアドレス】\n${email}\n【お問い合わせ内容】\n${message}`,
+        },
+      ],
+    }),
+  });
+
+  var response = await toAdminRes.json();
+  console.log(`status: ${toAdminRes.status}; response: ${response}`);
+
+  // お客様へ自動返信メール
+  if (toAdminRes.ok) {
+    console.log("お客様へ自動返信メール");
+    const toCustRes = await fetch("https://api.mailchannels.net/tx/v1/send", {
       method: "POST",
       headers: {
         "content-type": "application/json",
@@ -18,69 +45,33 @@ export default defineEventHandler(async (event) => {
       body: JSON.stringify({
         personalizations: [
           {
-            to: [{ email: recipientEmail }],
+            to: [{ email: email, name: name }],
+            dkim_domain: "pso2-search.com", // The value has to be the domain you added DKIM records to and where you're sending your email from
+            dkim_selector: "mailchannels",
+            dkim_private_key: `${config.DKIM_PRIVATE_KEY}`,
           },
         ],
         from: { email: senderEmail, name: sender },
-        subject: "お問い合わせがありました",
+        subject: "お問い合わせありがとうございます！",
         content: [
           {
             type: "text/plain",
-            value: `【お名前】\n${name}様\n【メールアドレス】\n${email}\n【お問い合わせ内容】\n${message}`,
+            value: `${name}様\nこの度はお問い合わせいただきありがとうございます。(略)\n【お問い合わせ内容】\n${message}`,
           },
         ],
       }),
     });
 
-    // お客様へ自動返信メール
-    if (toAdminRes.ok) {
-      console.log("お客様へ自動返信メール");
-      const toCustRes = await fetch("https://api.mailchannels.net/tx/v1/send", {
-        method: "POST",
-        headers: {
-          "content-type": "application/json",
-        },
-        body: JSON.stringify({
-          personalizations: [
-            {
-              to: [{ email: email, name: name }],
-              "dkim_domain": "pso2-search.com", // The value has to be the domain you added DKIM records to and where you're sending your email from
-              "dkim_selector": "mailchannels",
-              "dkim_private_key": `${config.DKIM_PRIVATE_KEY}`,
-            },
-          ],
-          from: { email: senderEmail, name: sender },
-          subject: "お問い合わせありがとうございます！",
-          content: [
-            {
-              type: "text/plain",
-              value: `${name}様\nこの度はお問い合わせいただきありがとうございます。(略)\n【お問い合わせ内容】\n${message}`,
-            },
-          ],
-        }),
-      });
+    var response = await toCustRes.json();
+    console.log(`status: ${toCustRes.status}; response: ${response}`);
 
-      if (toCustRes.ok) {
-        console.log("送信に成功しました！");
-        return "送信に成功しました！";
-      } else {
-        console.log(
-          `送信失敗。（Status = ${toCustRes.status}, Text = ${toCustRes.statusText}）`
-        );
-        throw createError({
-          statusCode: 500,
-        });
-      }
+    if (toCustRes.ok) {
+      console.log("送信成功");
+      return "送信に成功しました！";
     }
-    console.log("送信失敗。");
-    throw createError({
-      statusCode: 500,
-    });
-  } catch (error) {
-    console.log(`StackTrace: ${error}`);
-    throw createError({
-      statusCode: 500,
-      message: `${error}`,
-    });
   }
+  console.log("送信失敗。");
+  throw createError({
+    statusCode: 500,
+  });
 });
